d345d382e2989123bb01aa840b88f4ec6420cdb9
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-20 12:42:22 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function divide (a,b) {
+    return a/b;
+}
+
+// // Arrow function for square
+const square = (x) => {
+    return x * x;
+}
+
+// // Arrow function for add
+ const add = (a, b) => {
+    return a + b;
+}
